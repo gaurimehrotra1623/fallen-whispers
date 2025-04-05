7635b838caf796bf9c2d6a609ad49eca31072962
Gauri Mehrotra <gaurimehrotra18@gmail.com>
2025-04-06 02:24:42 +0530

    fixed home

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
     <div className='nav'>
       <div className="nav-logo">Fallen Whispers</div>
       <ul className="nav-menu">
-        <li><a href="/">Home</a></li>
+        <li>Home</li>
         <li><a href="#storyline">Storyline</a></li>
         <li><a href="#contact">Contact Us</a></li>
         <li className='nav-download'><a href="https://www.ubisoft.com/en-gb/game/assassins-creed" target='_blank'>Download</a></li>
